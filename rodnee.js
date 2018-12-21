ed89c9191b51f310d5d56600f436c9534a9ca8ac
rknucks <42450529+rknucks@users.noreply.github.com>
2018-12-20 20:19:20 -0700

    put in a for loop so more results would show

--- rodnee.js
+++ rodnee.js
@@ -14,7 +14,7 @@ $("#add-event").on("click", function(event) {
  
  
 var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" +
- event + "&size=1&apikey=exjiYSnDEt1bNf9JQHhvljoCD4tUdae2";
+ event + "&apikey=exjiYSnDEt1bNf9JQHhvljoCD4tUdae2";
 
     // Here we run our AJAX call to the Ticket Master API
     $.ajax({
@@ -23,12 +23,24 @@ var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" 
     })
       // We store all of the retrieved data inside of an object called "response"
       .then(function(response) {
+        for (var i = 0; i < response._embedded.events.length; i++) {
+            var eventName = response._embedded.events[i].name
+            var eventDate = response._embedded.events[i].dates.start.localDate
+            var eventVenue = response._embedded.events[i]._embedded.venues[0].name
+            var eventCity = response._embedded.events[i]._embedded.venues[0].city.name
 
 
-            var eventName = response._embedded.events[0].name
-            var eventDate = response._embedded.events[0].dates.start.localDate
-            var eventVenue = response._embedded.events[0]._embedded.venues[0].name
-            var eventCity = response._embedded.events[0]._embedded.venues[0].city.name
+            $("#show").append("<h1>Events: "+ eventName + " on " + eventDate + "</h1>" +
+            "<br>" +
+            
+            "<p>Where: " + eventCity + " At Venue: " + eventVenue + "</p>");
+           // $("#showNext").append("<p>Where: " + eventCity + " At Venue: " + eventVenue + "</p>")
+        }
+
+           // var eventName = response._embedded.events[0].name
+            //var eventDate = response._embedded.events[0].dates.start.localDate
+            //var eventVenue = response._embedded.events[0]._embedded.venues[0].name
+            //var eventCity = response._embedded.events[0]._embedded.venues[0].city.name  
 
 
         // Log the queryURL
@@ -37,11 +49,11 @@ var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" 
         // Log the resulting object
         console.log(response);
 
-        $("#show").html("<h1>Events: "+ eventName + " on " + eventDate + "</h1>");
-        console.log(response._embedded.events[0].dates.start.localDate);
+        //$("#show").html("<h1>Events: "+ eventName + " on " + eventDate + "</h1>");
+        //console.log(response._embedded.events[0].dates.start.localDate);
        
-        $("#showNext").html("<p>Where: " + eventCity + " At Venue: " + eventVenue + "</p>");
-        console.log(eventCity, eventVenue);
+        //$("#showNext").html("<p>Where: " + eventCity + " At Venue: " + eventVenue + "</p>");
+        //console.log(eventCity, eventVenue);
 
         
       });
