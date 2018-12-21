304d28a4cb0f965e3faff1bfc293c96eef4aa2ea
rknucks <42450529+rknucks@users.noreply.github.com>
2018-12-20 19:53:40 -0700

    working the event search

--- rodnee.js
+++ rodnee.js
@@ -24,14 +24,24 @@ var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?keyword=" 
       // We store all of the retrieved data inside of an object called "response"
       .then(function(response) {
 
+
+            var eventName = response._embedded.events[0].name
+            var eventDate = response._embedded.events[0].dates.start.localDate
+            var eventVenue = response._embedded.events[0]._embedded.venues[0].name
+            var eventCity = response._embedded.events[0]._embedded.venues[0].city.name
+
+
         // Log the queryURL
         console.log(queryURL);
 
         // Log the resulting object
         console.log(response);
 
-        $("#show").html("<h1>Events" + response.name + "</h1>");
-      $("#show").html("<p>" + response.event + "</p>");
+        $("#show").html("<h1>Events: "+ eventName + " on " + eventDate + "</h1>");
+        console.log(response._embedded.events[0].dates.start.localDate);
+       
+        $("#showNext").html("<p>Where: " + eventCity + " At Venue: " + eventVenue + "</p>");
+        console.log(eventCity, eventVenue);
 
         
       });
